7baf717adea3c18f3755166e6c894388ec9e368f
Clark5 <wtwang@rice.edu>
2023-02-26 23:43:23 -0600

    Add folders for results.

--- simulation/batch.sh
+++ simulation/batch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python run.py --cc poseidon --trace trace_multi_hop_congestion_small --bw 100 --topo topo_racks --poseidon_m 0.25 --poseidon_min_rate 1 > ./results/data/poseidon_0.25_test.txt
+python run.py --cc poseidon --trace trace_multi_hop_congestion_small --bw 100 --topo topo_racks --poseidon_m 0.25 --poseidon_min_rate 1.0 > ./results/data/poseidon_0.25_1.0.txt
